0f102a8c24964a66b7a45d926a573a0abdb207d5
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-02-09 23:34:30 -0400

    feat: add renderTodoTable from views/todoViews.js

--- src/views/todoView.js
+++ src/views/todoView.js
@@ -1,11 +1,11 @@
 // @ts-check
-import { elements } from "../views/base.js";
+import { elements } from "./elements.js";
 
 /**
  *
  * @param {import("../models/Todo.js").ITodo[]} todos
  */
-export function renderTodo(todos) {
+export function renderTodoList(todos) {
   let markup = "";
 
   todos.forEach(({ id, title, completed }) => {
@@ -14,11 +14,39 @@ export function renderTodo(todos) {
       <input name="completed" type="checkbox" data-todo="toggle" ${
         completed && "checked"
       }>
-      <b>${title}</b>
+      ${completed ? `<s>${title}</s>` : `${title}`}
     </li>
   `;
   });
 
   elements.list?.replaceChildren();
   elements.list?.insertAdjacentHTML("afterbegin", markup);
+}
+
+/**
+ *
+ * @param {import("../models/Todo.js").ITodo[]} todos
+ */
+export function renderTodoTable(todos) {
+  let markup = "";
+
+  todos.forEach(({ id, title, completed }) => {
+    markup += `
+      <tr id="${id}">
+          <td>${id}</td>
+          <td ${
+            completed && "style='text-decoration: line-through'"
+          }>${title}</td>
+          <td >
+            <input name="completed" type="checkbox" data-todo="toggle" ${
+              completed && "checked"
+            }>
+            </td>
+            <td><button data-todo="remove">Remove Todo</button></td>
+      </tr>
+  `;
+  });
+
+  elements.tableBody?.replaceChildren();
+  elements.tableBody?.insertAdjacentHTML("afterbegin", markup);
 }
