21af039ee4440c1edb7247765a7482a05f7d7bd6
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-02-09 23:32:49 -0400

    refactor: rename base.js to elements.js

--- src/views/elements.js
+++ src/views/elements.js
@@ -4,8 +4,17 @@ export const elements = {
   /** @type {HTMLInputElement | null} */
   input: document.querySelector("[data-todo='new']"),
 
+  /** @type {HTMLButtonElement | null} */
+  deleteButton: document.querySelector("[data-todo='delete']"),
+
+  /** @type {HTMLButtonElement | null} */
+  removeButton: document.querySelector("[data-todo='remove']"),
+
   /** @type {HTMLUListElement | null} */
   list: document.querySelector("[data-todo='list']"),
+
+  /** @type {HTMLTableElement | null} */
+  tableBody: document.querySelector("[data-todo='table-body']"),
 
   /** @type {HTMLInputElement | null} */
   checkbox: document.querySelector("[data-todo='toggle']"),
